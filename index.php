5e76e096d594ac267cc2590140b57bf36bcb25e6
Simon Kühn <simon@Paule.fritz.box>
2019-03-17 10:22:36 +0100

    More fancier error handling with showing the directory

--- index.php
+++ index.php
@@ -1,18 +1,28 @@
 <?php
 
+	error_reporting(E_ALL);
+
 // @author Simon Kühn
 //
 
-if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+$logFile = 'web.err.log';
+$logFileDir = '/home/sites/site32/logs/';
+$output = '<h1>Manitu Log File Analyser</h1>'."\n";
+$logFileString = '';
 
-$logFile = "/home/sites/site32/logs/web.err.log";
 
-
-if(file_exists($logFile)) {
-	echo "<pre>";
-	readfile($logFile);
-	echo "</pre>";
+if(file_exists($logFileDir.$logFile)) {
+	$logFileString = file_get_contents($logFileDir.$logFile);
+	$output .= "<h3>Content of " . $logFile . "</h3>\n";
+	$output .= "<pre>\n";
+	$output .= $logFileString;
+	$output .= "</pre>\n";
 } else {
-	var_dump(ABSPATH);
+	$output .= "<h3>Error: File does not exist. </h3>\n";
+	$output .= "<p>Showing directory instead.<p>\n";
+	$output .= "<ul>\n<li>";
+	$output .= implode("</li>\n<li>", scandir($logFileDir));
+	$output .= "</li>\n</ul>";
 }
+
+echo $output;
